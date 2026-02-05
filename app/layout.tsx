2906787af7b5101f9d571faec7a939f25bcb63cd
hardpress.dev <hardpress.dev@proton.me>
2026-02-05 12:28:28 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+//import { Analytics } from "@/components/analytics"
 import { Analytics } from "@vercel/analytics/react"
 import { ModeToggle } from "@/components/mode-toggle"
 
@@ -33,11 +34,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 </nav>
               </div>
             </header>
-            
             <main>{children}</main>
-            
-            {/* Removed flex-grow logic. Forced text-left and reduced margin significantly */}
-            <footer className="mt-8 pt-8 border-t border-slate-200 dark:border-slate-800 text-left text-sm text-slate-500 dark:text-slate-400">
+            <footer className="mt-24 text-center text-sm text-slate-500 dark:text-slate-400">
               ©hardpress.dev | 2026
             </footer>
           </div>
